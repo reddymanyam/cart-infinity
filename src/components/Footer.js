360f7223c1e04a2ff9cefb91afc099c2c0218feb
reddymanyam <reddappareddy18@gmail.com>
2024-07-05 22:43:52 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { blue } from '@mui/material/colors';
 
 const Footer = () => {
   return (
-    <Box
+    <Box position='sticky-bottom'
       component="footer"
       sx={{
         bgcolor: blue[500],
@@ -15,7 +15,7 @@ const Footer = () => {
     >
       <Container maxWidth="lg">
         <Typography variant="body2" color="inherit" align="center">
-          © {new Date().getFullYear()} Created by <a href='https://www.linkedin.com/in/reddappa-reddy-manyam-b7032b202/' style={{textDecoration:'none', cursor:'pointer', color:'yellow', fontSize:'15px'}}> Reddy Manyam </a>
+          © {new Date().getFullYear()} Created by <a href='https://www.linkedin.com/in/reddappa-reddy-manyam-b7032b202/' style={{textDecoration:'none', cursor:'pointer', color:'yellow', fontSize:'15px'}}> Reddy Manyam 💛</a>
         </Typography>
       </Container>
     </Box>
