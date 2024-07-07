9fa458fc20dbb640cedc2e2ed6d3e986069d0549
reddymanyam <reddappareddy18@gmail.com>
2024-07-07 22:35:03 +0530

    data is updated

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,7 +3,6 @@ export const items =  [
       id:1,
       category: 'mobiles',
        title:'Realme Note 12 pro',
-   
        price:'39999',
       imgSrc:"https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/l/x/c/-original-imagx6rdpmhuq5ba.jpeg?q=70&crop=false",
       description:"12 GB RAM | 512 GB ROM 16.94 cm (6.67 inch)200MP (OIS) + 8MP + 2MP "
@@ -12,175 +11,160 @@ export const items =  [
       id:2,
       category: 'mobiles',
       title:"Iphone 15 pro",
-   
-       price:'89999',
+      price:'89999',
       imgSrc:"https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/k/u/6/-original-imaghxen343tbjgj.jpeg?q=90",
       description:"128 GB ROM |15.49 cm (6.1 inch) Super Retina XDR Display |48MP + 12MP + 12MP | 12MP"
   },
   {   id:3,
       category: 'mobiles',
-       title:'Redmi note 13 pro',
-
-       price:'29999',
+      title:'Redmi note 13 pro',
+      price:'29999',
       imgSrc:"https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/p/8/i/-original-imagwu89u7fgsq4b.jpeg?q=70&crop=false",
       description:'12 GB RAM | 512 GB ROM |16.94 cm (6.67 inch) Display | 200MP (OIS) + 8MP + 2MP | 16MP'
   },{ 
       id:4,
       category: 'mobiles',
       title:'Realme c3',
-    
-       price:'22999',
+      price:'22999',
       imgSrc:"https://rukminim2.flixcart.com/image/850/1000/k6jnfrk0/cases-covers/back-cover/k/v/v/flipkart-smartbuy-fsb-rel-c3-p185-original-imafzzgkzezj757h.jpeg?q=90",
       description:'12 GB RAM | 512 GB ROM 16.94 cm (6.67 inch)200MP (OIS) + 8MP + 2MP '
   },{
     id:5,
     category: "EarPods",
     title:'Boult Z40 Bluetooth Headset ',
-   
-     price:'2499',
+    price:'2499',
     imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/headphone/q/q/n/-original-imagmugg8hx9v5fr.jpeg?q=70",
     description:'Scratch Proof Design | Rubber Grip Dual Tone Case.'
 },
 {  id:6,
-  category: "EarPods",
+    category: "EarPods",
     title:"boAt Airdrops",
-  
-     price:'1999',
+    price:'1999',
     imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/headphone/4/o/t/-original-imagwucfqr8tn72n.jpeg?q=70",
     description:"boAt Airdrops scratch Proof Design | Rubber Grip Dual Tone Case."
 },
 {   id:7,
-  category: "EarPods",
+    category: "EarPods",
     title:'Aroma NB119 ',
-
-     price:'2499',
+    price:'2499',
     imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/headphone/t/z/1/nb140-fabulous-24-hours-playtime-deep-bass-made-in-india-original-imagmzhaqrddnfuz.jpeg?q=70",
     description:'Aroma NB119 Titanium Scratch Proof Design | Rubber Grip'
 },{
     id:8,
     category: "EarPods",
     title:'Boult Z40 premium',
-
-     price:'1999',
+    price:'1999',
     imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/headphone/o/z/8/-original-imagtr2n2wzggxuh.jpeg?q=70",
     description:'Boult Z40 Scratch Proof Design | Rubber Grip Dual Tone Case.'
 },
   {   id:9,
       category: "men's",
       title:"Men's Jacket" ,
-      
-       price:'1499',
+      price:'1499',
       imgSrc:"https://onemile.in/cdn/shop/files/Hyperflex_Dusty_Shacket.png?v=1704718951",
       description:"Men's jacket | Winter wear and Winter Collection"
   },
   {   id:10,
-    category: "men's",
+      category: "men's",
       title:"Men's T-Shirt",
-   
-       price:'599',
+      price:'599',
       imgSrc:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1687842433_3385347.jpg?w=480&dpr=1.5",
       description:"Men's T-Shirt | Special PriceGet extra 10% off on 20 item(s)"
   },{
       id:11,
       category: "men's",
       title:"Men's Printed Shirt",
-    
-       price:'899',
+      price:'899',
       imgSrc:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698729750_8605780.jpg?format=webp&w=480&dpr=1.5",
       description:"Men's Printed Shirt | On beach day and casual wear"
   },{ 
       id:12,
       category: "men's",
       title:"Men's Shirt",
- 
-       price:'799',
+      price:'799',
       imgSrc:"https://thefoomer.in/cdn/shop/files/jpeg-optimizer_PATP7026.jpg?v=1683812028&width=1200",
       description:"Men's Shirt | Casual wear and Party wear"
   },{
       id:13,
       category: "women's",
       title:"Women's Blue saree",
-     
-       price:'2599',
+      price:'2599',
       imgSrc:"https://static.heerfashion.com/public/products/10-2021/productID809zua9tHO2eKKCTVxjGrDNy3aab1uY8e9bwZuGa.jpeg",
       description:"Women's Blue saree | Special PriceGet extra 5% off on 10 item(s)",
   } , { id:14,
       category: "women's",
       title:"Girl's modren dress",
-  
-       price:'1199',
+      price:'1199',
       imgSrc:"https://5.imimg.com/data5/SELLER/Default/2023/4/304408297/WA/AP/NF/141462711/lakhnavi-vol-4-dark-color-heavy-rayon-with-all-over-white-thread-work-kurti-with-plazo-and-dupatta-500x500.jpeg",
       description:"Girl's modren dress | Special PriceGet extra 10% off on 20 item(s)"
   },{
       id:15,
       category: "women's",
       title:"Girl's modren dress",
-   
-       price:'2599',
+      price:'2599',
       imgSrc:"https://static3.azafashions.com/tr:w-317/uploads/product_gallery/1704981742608_1.jpg?noopt=true",
       description:"Girl's modren dress | Special PriceGet extra 15% off on 15 item(s)"
   },{
       id:16,
       category: "women's",
       title:"Women's White saree",
-    
-       price:'1699',
+      price:'1699',
       imgSrc:"https://images.meesho.com/images/products/149836403/0kzlh_512.webp",
       description:"Women's White saree | Special PriceGet extra 10% off on 20 item(s)"
   },
   {   id:17,
-    category: "Sports",
+      category: "Sports",
       title:"KOOKABURRA bat" ,
-       price:'699',
+      price:'699',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/kbv4fww0/bat/y/h/q/1-6-kookaburra-kahuna-full-size-poplar-willow-cricket-bat-1106-original-imaft3r7hqbmhj46.jpeg?q=70",
       description:"KOOKABURRA KB Kahuna 800 English Willow Cricket Bat  (1200)"
   },
   {   id:18,
-    category: "Sports",
+      category: "Sports",
       title:"YONEX GR 303i ",
-       price:'899',
+      price:'899',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/racquet/y/5/f/-original-imagwyubzmhyvkjc.jpeg?q=70",
       description:"YONEX GR303i Red Strung Badminton Racquet  (Pack of: 1, 95 g)"
   },{
       id:19,
       category: "Sports",
       title:"Arrow Set for Kids",
-       price:'599',
+      price:'599',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/toy-sport/x/h/z/5-archery-bow-and-arrow-toy-set-with-target-board-3-suction-cup-original-imaghnzhnzntkym3.jpeg?q=70",
       description:"Planet of Toys Heavy Duty Bow and Arrow Set for Kids Age 5 -12 Years Old Archery Kit"
   },{
       id:20,
       category: "Sports",
       title:"Basket Ball Kit",
-       price:'999',
+      price:'999',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/ball/u/q/w/400-500-basket-ball-7-no-basketball-with-basketball-ring-7-7-2-original-imag88j2cxyqrv9g.jpeg?q=70",
       description:"Addiction Heavy Basketball Stand with net and Size: 7  (Pack of 1) | Rubber Moulded "
   },{
       id:21,
       category: "Books",
       title:"Rich Dad Poor Dad",
-       price:'299',
+      price:'299',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/book/k/2/r/rich-dad-poor-dad-what-the-rich-teach-their-kids-about-money-original-imagtcggjfxuryva.jpeg?q=70",
       description:"Rich DAD Poor DAD | Author: Robert Kiyosaki 547 Pages|Language-English "
   },{
       id:22,
       category: "Books",
       title:"One Indian Girl",
-       price:'349',
+      price:'349',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/xif0q/book/5/f/w/-original-imagqtdt8czakvzm.jpeg?q=70",
       description:"One Indian Girl | Author: Chetan Bhagat 336 Pages|Language-English "
   },{
       id:23,
       category: "Books",
       title:"Wings Of Fire",
-       price:'749',
+      price:'749',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/kjkbv680-0/book/p/r/m/wings-of-fire-original-imafz3pawzphkecj.jpeg?q=70",
       description:"Wings Of Fire | Author: A.P.J 380 Pages|Language-English "
   },{
       id:24,
       category: "Books",
       title:"Reminders Of Him",
-       price:'199',
+      price:'199',
       imgSrc:"https://rukminim2.flixcart.com/image/612/612/l2m78280/book/o/c/s/reminders-of-him-by-colleen-hoover-english-paperback-original-imagdx9f3z4ufzqw.jpeg?q=70",
       description:"Reminders Of Him | Author: Colleen Hoover 336 Pages|Language-English "
   },{
